bffc1fbd69b8f1a17325604dd9a29a50d8b89dd1
vincent <hirvin42@gmail.com>
2018-02-01 14:58:08 +0100

    hello

---
+++ inc/config.inc.php
@@ -0,0 +1,8 @@
+<?php
+return array(
+   'db.host' => 'localhost',
+   'db.port' => 3306,
+   'db.username' => 'root',
+   'db.password' => 'dautremepuis42',
+   'db.name' => "carnet-d'adresse",
+);
