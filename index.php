52cd3f57352bf33ab4d8c96cadc7595e588a3057
vincent42100 <34571021+vincent42100@users.noreply.github.com>
2018-02-01 15:07:58 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+include 'inc/autoload.inc';
+?>
+<?php
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>First PHP with Eclipse</title>
+  <link rel="stylesheet" href="styles/style.css">
+</head>
+<body>
+ 
+ <h1> bonjour </h1>
+ 
+</body>
+</html>
